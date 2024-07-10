ce8b389b23e71e2e8cc55d06cb854a166cc35272
JulianIgnacio <montenegrojulianig@gmail.com>
2024-07-10 09:19:08 -0300

    se configuro login

--- Laboratorio3/controllers/UsuariosControler.js
+++ Laboratorio3/controllers/UsuariosControler.js
@@ -1,4 +1,5 @@
 const {conection} = require("../config/DB")
+const {axios} = require("axios")
 
 
 const allUsers = (req,res) =>{
@@ -9,17 +10,30 @@ const allUsers = (req,res) =>{
     })
 }
 
-const login = (req,res) => {
+const login = async (req,res) => {
 
-    const nombreUsuario = req.params.nombreUsuario
-    const contraseña = req.body.contraseña
+    const nombreUsuario = req.body.nombreUsuario;
+    const contraseña = req.body.contraseña;
 
-    const query = `select * from Usuarios where nombreUsuario = '${nombreUsuario}' and contraseña='${contraseña}'`
-    conection.query(query,(err,results)=>{
+    const query = 'SELECT * FROM Usuarios WHERE nombreUsuario = ?';
+    conection.query(query, [nombreUsuario], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err });
+        }
 
-        if(err) throw err;
-        res.status(200).json({message:"inicio de sesion exitoso"})
-    })
+        if (results.length === 0) {
+            return res.status(401).json({ message: 'Usuario o contraseña erroneo' });
+        }
+
+        const usuario = results[0];
+
+        // Comparar la contraseña en texto plano (no recomendado)
+        if (contraseña === usuario.contraseña) {
+            res.status(200).json({ message: 'Inicio de sesión exitoso' });
+        } else {
+            res.status(401).json({ message: 'Usuario o contraseña erroneo' });
+        }
+    });
 }
 
 module.exports = {allUsers,login}
--- Laboratorio3/index.js
+++ Laboratorio3/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const {conection} = require("./config/DB")
 const cors= require("cors")
+const bodyParser = require('body-parser');
 
 const Usuarios = require("./routes/UsuariosRoute")
 const Empleados = require("./routes/EmpleadosRoute")
@@ -17,6 +18,9 @@ const port = 8000;
 
 app.use(express.json())
 app.use(cors())
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/",Usuarios)
 app.use("/",Empleados)
